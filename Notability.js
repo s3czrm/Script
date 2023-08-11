ad5e3446669bcfaf2827c2e37a1cee07c432713e
Yu9191 <132221547+Yu9191@users.noreply.github.com>
2023-08-11 14:57:39 +0800

    Create Notability.js

---
+++ Notability.js
@@ -0,0 +1,47 @@
+
+/*
+
+@：Notability
+@：https://t.cn/A6Cgjtei
+@：chxm1023
+
+
+**************************************
+
+[rewrite_local]
+^https?:\/\/notability\.com\/(global|subscriptions) url script-response-body https://raw.githubusercontent.com/Yu9191/Script/main/Notability.js
+
+[mitm]
+hostname = notability.com
+
+*/
+
+
+var baby = JSON.parse($response.body);
+
+baby = {
+   "data" : {
+     "processAppleReceipt" : {
+       "error" : 0,
+       "subscription" : {
+         "productId" : "com.gingerlabs.Notability.premium_subscription",
+         "originalTransactionId" : "570001184068302",
+         "tier" : "premium",
+         "refundedDate" : null,
+         "refundedReason" : null,
+         "isInBillingRetryPeriod" : false,
+         "expirationDate" : "2099-09-09T09:09:09.000Z",
+         "gracePeriodExpiresAt" : null,
+         "overDeviceLimit" : false,
+         "expirationIntent" : null,
+         "__typename" : "AppStoreSubscription",
+         "user" : null,
+         "status" : "canceled",
+         "originalPurchaseDate" : "2022-09-09T09:09:09.000Z"
+       },
+       "__typename" : "SubscriptionResult"
+    }
+  }
+};
+
+$done({body : JSON.stringify(baby)});
